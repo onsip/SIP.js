c4aa26835c58f362243e9a456e33301a71a7f99b
Eric Green <eric.green@onsip.com>
2013-07-23 11:16:44 -0400

    Remove deprecated onicechange function. Replaced deprecated ongatheringchange with oniceconnectionstatechange.

--- src/RTCSession/RTCMediaHandler.js
+++ src/RTCSession/RTCMediaHandler.js
@@ -148,8 +148,8 @@ RTCMediaHandler.prototype = {
       }
     };
 
-    // To be deprecated as per https://code.google.com/p/webrtc/issues/detail?id=1393
-    this.peerConnection.ongatheringchange = function(e) {
+    this.peerConnection.oniceconnectionstatechange = function(e) {
+      self.logger.log('ICE connection state changed to "'+ this.iceConnectionState +'"');
       if (e.currentTarget.iceGatheringState === 'complete' && this.iceConnectionState !== 'closed') {
         self.onIceCompleted();
       }
