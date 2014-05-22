2ca8cbd40bb55a05f6206c13a6833277df5edbea
Will Mitchell <will@onsip.com>
2014-05-21 20:52:39 -0400

    Remove silly comment

--- src/WebRTC.js
+++ src/WebRTC.js
@@ -16,7 +16,6 @@ WebRTC.getUserMedia = SIP.Utils.getPrefixedProperty(window.navigator, 'getUserMe
 WebRTC.RTCPeerConnection = SIP.Utils.getPrefixedProperty(window, 'RTCPeerConnection');
 WebRTC.RTCSessionDescription = SIP.Utils.getPrefixedProperty(window, 'RTCSessionDescription');
 
-// isSupported attribute.
 if (WebRTC.getUserMedia && WebRTC.RTCPeerConnection && WebRTC.RTCSessionDescription) {
   WebRTC.getUserMedia = WebRTC.getUserMedia.bind(window.navigator);
   WebRTC.isSupported = true;
