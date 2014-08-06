0862df7c825b27f90a5665f584334b232eddf644
Joseph Frazier <joseph@onsip.com>
2014-08-05 20:30:19 -0400

    Cordova plugin uses src/SIP.js as its module
    This addresses #83 as described in item 1 of https://github.com/onsip/SIP.js/issues/83#issue-39390817

---
+++ cordova_package.json.js
@@ -0,0 +1,9 @@
+// This file contains the "title" and "version" of package.json. This allows
+// src/SIP.js to require('../package.json'), but still be used as a Cordova
+// module.  cordova.require doesn't support json files, but plugin.xml tells
+// Cordova that package.json is actually this file, so src/SIP.js still has
+// access to the fields it needs.
+module.exports = {
+  title: "SIP.js",
+  version: "0.6.2"
+};
