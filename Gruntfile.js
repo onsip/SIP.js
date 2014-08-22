dea742b5dcde82117a6253b5cd1523f2439ff837
Joseph Frazier <joseph@onsip.com>
2014-08-22 18:31:52 -0400

    tests use promiscuous 0.6.0 for Promise
    curl https://raw.githubusercontent.com/RubenVerborgh/promiscuous/dist/promiscuous-browser.js > test/polyfills/promiscuous-browser.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,6 +99,7 @@ module.exports = function(grunt) {
         options: {
           specs: 'test/spec/*.js',
           keepRunner : true,
+          vendor: 'test/polyfills/*.js',
           helpers: 'test/helpers/*.js'
         }
       }
