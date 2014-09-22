1e4ac0f8efc4eae21a7e7d216b0161fa4fd99274
Joseph Frazier <joseph@onsip.com>
2014-09-22 06:30:14 -0400

    src/SIP.js: fix indentation

--- src/SIP.js
+++ src/SIP.js
@@ -2,45 +2,45 @@
  * @name SIP
  * @namespace
  */
-  "use strict";
+"use strict";
 
-  var SIP = {};
-  module.exports = SIP;
+var SIP = {};
+module.exports = SIP;
 
-  var pkg = require('../package.json');
+var pkg = require('../package.json');
 
-  Object.defineProperties(SIP, {
-    version: {
-      get: function(){ return pkg.version; }
-    },
-    name: {
-      get: function(){ return pkg.title; }
-    }
-  });
+Object.defineProperties(SIP, {
+  version: {
+    get: function(){ return pkg.version; }
+  },
+  name: {
+    get: function(){ return pkg.title; }
+  }
+});
 
-  require('./Utils')(SIP);
-  SIP.LoggerFactory = require('./LoggerFactory');
-  require('./EventEmitter')(SIP);
-  SIP.C = require('./Constants')(SIP.name, SIP.version);
-  SIP.Exceptions = require('./Exceptions');
-  SIP.Timers = require('./Timers');
-  require('./Transport')(SIP, global.WebSocket);
-  require('./Parser')(SIP);
-  require('./SIPMessage')(SIP);
-  require('./URI')(SIP);
-  require('./NameAddrHeader')(SIP);
-  require('./Transactions')(SIP);
-  require('./Dialogs')(SIP);
-  require('./RequestSender')(SIP);
-  require('./RegisterContext')(SIP);
-  SIP.MediaHandler = require('./MediaHandler')(SIP.EventEmitter);
-  require('./ClientContext')(SIP);
-  require('./ServerContext')(SIP);
-  require('./Session')(SIP);
-  require('./Subscription')(SIP);
-  SIP.WebRTC = require('./WebRTC')(SIP);
-  require('./UA')(SIP);
-  SIP.Hacks = require('./Hacks');
-  require('./SanityCheck')(SIP);
-  SIP.DigestAuthentication = require('./DigestAuthentication')(SIP.Utils);
-  SIP.Grammar = require('./Grammar/dist/Grammar')(SIP);
+require('./Utils')(SIP);
+SIP.LoggerFactory = require('./LoggerFactory');
+require('./EventEmitter')(SIP);
+SIP.C = require('./Constants')(SIP.name, SIP.version);
+SIP.Exceptions = require('./Exceptions');
+SIP.Timers = require('./Timers');
+require('./Transport')(SIP, global.WebSocket);
+require('./Parser')(SIP);
+require('./SIPMessage')(SIP);
+require('./URI')(SIP);
+require('./NameAddrHeader')(SIP);
+require('./Transactions')(SIP);
+require('./Dialogs')(SIP);
+require('./RequestSender')(SIP);
+require('./RegisterContext')(SIP);
+SIP.MediaHandler = require('./MediaHandler')(SIP.EventEmitter);
+require('./ClientContext')(SIP);
+require('./ServerContext')(SIP);
+require('./Session')(SIP);
+require('./Subscription')(SIP);
+SIP.WebRTC = require('./WebRTC')(SIP);
+require('./UA')(SIP);
+SIP.Hacks = require('./Hacks');
+require('./SanityCheck')(SIP);
+SIP.DigestAuthentication = require('./DigestAuthentication')(SIP.Utils);
+SIP.Grammar = require('./Grammar/dist/Grammar')(SIP);
