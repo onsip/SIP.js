229cea9d7699b39ca188760de33a7cdba1fd296d
Ptah Dunbar <yarr@piratedunbar.com>
2014-09-23 23:56:05 -0400

    Where should naviagtor be defined?

--- src/Hacks.js
+++ src/Hacks.js
@@ -66,7 +66,7 @@ var Hacks = module.exports = {
   },
 
   Chrome: {
-    is_opera : (navigator.userAgent.match(/Opera|OPR\//) ? true : false),
+    is_opera : (navigator.userAgent.match(/Opera|OPR\//) ? true : false), // jshint ignore:line
 
     maskDTLS: function (message) {
       if (!this.is_opera && message.body) {
