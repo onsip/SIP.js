8a538896bf268f8760eb35b628abac0eca62cd3f
Joseph Frazier <joseph@onsip.com>
2014-10-07 00:37:57 -0400

    Split src/environment.js into Node/browser versions
    src/environment_browser.js:
      Use global variables instead of requiring modules. This undoes the effects of
      the following commits, removing ~1KB from the bundle size:
        51003e3: Timers: require('timers') instead of directly using global
        8baf989: LoggerFactory: require('console') instead of directly using global
        b69fef8: Utils.Promise = global.Promise || require('promiscuous');
    
    src/environment.js:
      Export a Node-compatible environment by extending the browser environment
      with the modules listed above.
    
    Gruntfile.js: Tests polyfill Promise using promiscuous-browser.js

--- src/environment.js
+++ src/environment.js
@@ -1,31 +1,9 @@
-function getPrefixedProperty (object, name) {
-  if (object == null) {
-    return;
-  }
-  var capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
-  var prefixedNames = [name, 'webkit' + capitalizedName, 'moz' + capitalizedName];
-  for (var i in prefixedNames) {
-    var property = object[prefixedNames[i]];
-    if (property) {
-      return property.bind(object);
-    }
-  }
-}
+var extend = require('util')._extend;
 
-module.exports = {
-  WebSocket: global.WebSocket,
-  open: global.open,
+extend(exports, require('./environment_browser'));
+
+extend(exports, {
   Promise: global.Promise || require('promiscuous'),
   console: require('console'),
-  timers: require('timers'),
-
-  MediaStream: getPrefixedProperty(global, 'MediaStream'),
-  getUserMedia: getPrefixedProperty(global.navigator, 'getUserMedia'),
-  RTCPeerConnection: getPrefixedProperty(global, 'RTCPeerConnection'),
-  RTCSessionDescription: getPrefixedProperty(global, 'RTCSessionDescription'),
-
-  attachMediaStream: global.attachMediaStream,
-  // these aren't prefixed, but some platforms don't have global.URL
-  createObjectURL: getPrefixedProperty(global.URL, 'createObjectURL'),
-  revokeObjectURL: getPrefixedProperty(global.URL, 'revokeObjectURL')
-};
+  timers: require('timers')
+});
--- src/polyfills/console.js
+++
@@ -1,7 +0,0 @@
-// Console is not defined in ECMAScript, so just in case...
-module.exports = global.console || {
-  debug: function () {},
-  log: function () {},
-  warn: function () {},
-  error: function () {}
-};
