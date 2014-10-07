16bb9d0e44ddc50ce39fe16aba066e187b6dd329
Joseph Frazier <joseph@onsip.com>
2014-10-07 02:04:47 -0400

    (Node) Use 'ws' module for WebSocket

--- src/environment.js
+++ src/environment.js
@@ -3,6 +3,7 @@ var extend = require('util')._extend;
 extend(exports, require('./environment_browser'));
 
 extend(exports, {
+  WebSocket: require('ws'),
   Promise: global.Promise || require('promiscuous'),
   console: require('console'),
   timers: require('timers')
