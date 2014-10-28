2127f024c88fcb6c8bcd8b1d2d3d31ebafb9cd97
Joseph Frazier <joseph@onsip.com>
2014-10-28 13:38:16 -0400

    src/Grammar/index.js: fix path to Grammar.pegjs
    This allows require('sip.js') to work correctly from other modules.

--- src/Grammar/index.js
+++ src/Grammar/index.js
@@ -1,6 +1,7 @@
 var options = require('./peg.json');
 var fs = require('fs');
-var grammar = fs.readFileSync('./src/Grammar/src/Grammar.pegjs').toString();
+var path = require('path').join(__dirname, './src/Grammar.pegjs');
+var grammar = fs.readFileSync(path).toString();
 var PEG = require('pegjs');
 var Grammar = PEG.buildParser(grammar, options);
 
