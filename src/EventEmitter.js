0c2e23a80a5716258cd06b1e645e9cf772b4f183
Joseph Frazier <joseph@onsip.com>
2014-12-14 22:31:39 -0500

    src/EventEmitter.js: correctly inherit from Node's EventEmitter
    https://github.com/joyent/node/wiki/Api-changes-between-v0.8-and-v0.10

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -2,9 +2,18 @@ var NodeEventEmitter = require('events').EventEmitter;
 
 module.exports = function (console) {
 
-function EventEmitter () {}
+function EventEmitter () {
+  NodeEventEmitter.call(this);
+}
 
-EventEmitter.prototype = Object.create(NodeEventEmitter.prototype);
+EventEmitter.prototype = Object.create(NodeEventEmitter.prototype, {
+  constructor: {
+    value: EventEmitter,
+    enumerable: false,
+    writable: true,
+    configurable: true
+  }
+});
 
 EventEmitter.prototype.off = function off (eventName, listener) {
   var warning = '';
