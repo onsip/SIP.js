onsip/SIP.js

commit d70d06730a1057757e0c937fd5f2af048e7131bb

Media Handler: renderHint local/remote is an (Array of) HTMLMediaElement
MediaStreamManager.render renders each MediaStream to the corresponding
HTMLMediaElement in the array, wrapping around if needed.

This removes one layer of nesting from the invite/accept mediaHint.

Note that <video>s no longer get muted if an <audio> is provided alongside.

Usage (compare with 4096365):

  var session = new SIP.UA().invite('welcome@onsip.com', {
    media: {
      render: {
        remote: [
          document.createElement('audio'), // included to demo array support
          document.createElement('video')
        ],
        local: document.createElement('video')
      }
    }
  });

src/WebRTC/MediaStreamManager.js
@@ -32,18 +32,28 @@ MediaStreamManager.streamId = function (stream) {
     .join('');
 };
 
+/**
+ * @param {(Array of) MediaStream} streams - The streams to render
+ *
+ * @param {(Array of) HTMLMediaElement} elements
+ *        - The <audio>/<video> element(s) that should render the streams
+ *
+ * Each stream in streams renders to the corresponding element in elements,
+ * wrapping around elements if needed.
+ */
 MediaStreamManager.render = function render (streams, elements) {
-  // only render first stream, see pull request #76
-  streams = [].concat(streams);
-  var stream = streams[0];
-  if (!elements || !stream) {
+  if (!elements) {
     return false;
   }
+  if (Array.isArray(elements) && !elements.length) {
+    throw new TypeError('elements must not be empty');
+  }
 
-  function attachAndPlay (element, stream) {
-    if (typeof element === 'function') {
-      element = element();
+  function attachAndPlay (elements, stream, index) {
+    if (typeof elements === 'function') {
+      elements = elements();
     }
+    var element = elements[index % elements.length];
     (environment.attachMediaStream || attachMediaStream)(element, stream);
     ensureMediaPlaying(element);
   }
@@ -73,15 +83,10 @@ MediaStreamManager.render = function render (streams, elements) {
     }, interval);
   }
 
-  if (elements.video) {
-    if (elements.audio) {
-      elements.video.volume = 0;
-    }
-    attachAndPlay(elements.video, stream);
-  }
-  if (elements.audio) {
-    attachAndPlay(elements.audio, stream);
-  }
+  // [].concat "casts" `elements` into an array
+  // so forEach works even if `elements` was a single element
+  elements = [].concat(elements);
+  [].concat(streams).forEach(attachAndPlay.bind(null, elements));
 };
 
 MediaStreamManager.prototype = Object.create(SIP.EventEmitter.prototype, {

commit 1faf0f13201a268ae537acd28e1ada8c61bafe5d

Reallow passing functions as media render hint
Closes https://github.com/onsip/SIP.js/issues/329

Kudos to @matthieusieben for noticing this. It was broken by
commit d70d06730a1057757e0c937fd5f2af048e7131bb.

Here's a test you can run within `npm run repl`:

    var session = new SIP.UA().invite('welcome@onsip.com', {
      media: {
        render: {
          remote: [
            () => document.createElement('audio'),
          ],
        }
      }
    });

src/WebRTC/MediaStreamManager.js
@@ -75,10 +75,10 @@ MediaStreamManager.render = function render (streams, elements) {
   }
 
   function attachAndPlay (elements, stream, index) {
-    if (typeof elements === 'function') {
-      elements = elements();
-    }
     var element = elements[index % elements.length];
+    if (typeof element === 'function') {
+      element = element();
+    }
     (environment.attachMediaStream || attachMediaStream)(element, stream);
     ensureMediaPlaying(element);
   }