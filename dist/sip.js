5272f62b73cbc34c0b2c41fa200e91fc0999f141
aaker <c.aaker@gmail.com>
2016-12-13 11:13:46 -0800

    dist folder updates for added debug.

--- dist/sip.js
+++ dist/sip.js
@@ -5253,10 +5253,13 @@ Session.prototype = {
       request.reply(415);
       return;
     }
-
+    console.log("receiveReinvite request");
+    console.log(request);
     this.mediaHandler.setDescription(request.body)
     .then(this.mediaHandler.getDescription.bind(this.mediaHandler, this.mediaHint))
     .then(function(body) {
+      console.log("receiveReinvite body");
+      console.log(body);
       request.reply(200, null, ['Contact: ' + self.contact], body,
         function() {
           self.status = C.STATUS_WAITING_FOR_ACK;
@@ -5275,8 +5278,14 @@ Session.prototype = {
     })
     .catch(function onFailure (e) {
       var statusCode;
+      console.log("receiveReinvite onFailure e");
+      console.log(e);
       if (e instanceof SIP.Exceptions.GetDescriptionError) {
-        statusCode = 500;
+
+        console.log("receiveReinvite onFailure GetDescriptionError");
+        console.log(e);
+
+        statusCode = 200;
       } else {
         self.logger.error(e);
         statusCode = 488;
@@ -11095,9 +11104,11 @@ MediaHandler.prototype = Object.create(SIP.MediaHandler.prototype, {
      * 2. addStreams
      * 3. createOffer/createAnswer
      */
-
+     console.log("getDescription self");
+     console.log(self);
     var streamPromise;
     if (self.localMedia) {
+
       self.logger.log('already have local media');
       streamPromise = SIP.Utils.Promise.resolve(self.localMedia);
     }
