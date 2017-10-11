47308a82b74d6026c3f40f964a99f167da6c58ab
Eric Green <eric.green@onsip.com>
2017-10-11 13:27:15 -0400

    Uglify ascii_only mode Add license back to distributables

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,41 @@
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var webpack = require('webpack');
+
+var pkg = require('./package.json');
+var year = new Date().getFullYear();
+var banner = '\
+\n\
+ SIP version ' + pkg.version + '\n\
+ Copyright (c) 2014-' + year + ' Junction Networks, Inc <http://www.onsip.com>\n\
+ Homepage: http://sipjs.com\n\
+ License: http://sipjs.com/license/\n\
+\n\
+\n\
+ ~~~SIP.js contains substantial portions of JsSIP under the following license~~~\n\
+ Homepage: http://jssip.net\n\
+ Copyright (c) 2012-2013 José Luis Millán - Versatica <http://www.versatica.com>\n\
+\n\
+ Permission is hereby granted, free of charge, to any person obtaining\n\
+ a copy of this software and associated documentation files (the\n\
+ "Software"), to deal in the Software without restriction, including\n\
+ without limitation the rights to use, copy, modify, merge, publish,\n\
+ distribute, sublicense, and/or sell copies of the Software, and to\n\
+ permit persons to whom the Software is furnished to do so, subject to\n\
+ the following conditions:\n\
+\n\
+ The above copyright notice and this permission notice shall be\n\
+ included in all copies or substantial portions of the Software.\n\
+\n\
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,\n\
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF\n\
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\n\
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE\n\
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION\n\
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION\n\
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\
+\n\
+ ~~~ end JsSIP license ~~~\n\
+\n\n\n';
 
 module.exports = {
   entry: {
@@ -21,7 +58,7 @@ module.exports = {
           // eslint options (if necessary)
         }
       },
-      { 
+      {
         test: /\.pegjs$/,
         loader: 'pegjs-loader',
         options: {
@@ -67,7 +104,13 @@ module.exports = {
   },
   plugins: [
     new UglifyJSPlugin({
-      include: ['sip.min.js']
+      include: ['sip.min.js'],
+      output: {
+        ascii_only: true
+      }
+    }),
+    new webpack.BannerPlugin({
+      banner: banner
     })
   ]
 };
