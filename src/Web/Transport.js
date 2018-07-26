e8fadcbb6369893c6c46e858d5698a0d693dd84f
Christian Schmidt <github@chsc.dk>
2018-07-26 10:15:57 +0200

    Reset keep-alive debounce timer

--- src/Web/Transport.js
+++ src/Web/Transport.js
@@ -438,6 +438,7 @@ Transport.prototype = Object.create(SIP.Transport.prototype, {
     }
 
     this.keepAliveDebounceTimeout = SIP.Timers.setTimeout(function() {
+      this.clearKeepAliveTimeout();
       this.emit('keepAliveDebounceTimeout');
     }.bind(this), this.configuration.keepAliveDebounce * 1000);
 
