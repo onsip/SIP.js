d5e90bd6517664719e1e2deb51ab128d35b9ef33
Eric Green <eric.green@onsip.com>
2018-11-08 09:04:59 -0500

    Stop fixing deprecated underscored UA options

--- src/UA.js
+++ src/UA.js
@@ -870,32 +870,10 @@ UA.prototype.loadConfig = function(configuration) {
       allowOutOfDialogRefers: false,
     };
 
-  // Pre-Configuration
-  function aliasUnderscored (parameter, logger) {
-    var underscored = parameter.replace(/([a-z][A-Z])/g, function (m) {
-      return m[0] + '_' + m[1].toLowerCase();
-    });
-
-    if (parameter === underscored) {
-      return;
-    }
-
-    var hasParameter = configuration.hasOwnProperty(parameter);
-    if (configuration.hasOwnProperty(underscored)) {
-      logger.warn(underscored + ' is deprecated, please use ' + parameter);
-      if (hasParameter) {
-        logger.warn(parameter + ' overriding ' + underscored);
-      }
-    }
-
-    configuration[parameter] = hasParameter ? configuration[parameter] : configuration[underscored];
-  }
-
   var configCheck = this.getConfigurationCheck();
 
   // Check Mandatory parameters
   for(parameter in configCheck.mandatory) {
-    aliasUnderscored(parameter, this.logger);
     if(!configuration.hasOwnProperty(parameter)) {
       throw new SIP.Exceptions.ConfigurationError(parameter);
     } else {
@@ -911,7 +889,6 @@ UA.prototype.loadConfig = function(configuration) {
 
   // Check Optional parameters
   for(parameter in configCheck.optional) {
-    aliasUnderscored(parameter, this.logger);
     if(configuration.hasOwnProperty(parameter)) {
       value = configuration[parameter];
 
