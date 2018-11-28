fa9364f9618e3492e57dbf110b40f1ecb45559d9
Eric Green <eric.green@onsip.com>
2018-11-28 14:07:25 -0500

    Move ReferServerContext into session.d.ts

--- types/session.d.ts
+++ types/session.d.ts
@@ -2,7 +2,7 @@ import { EventEmitter } from "./event-emitter";
 import { SessionDescriptionHandler, SessionDescriptionHandlerOptions, SessionDescriptionHandlerModifiers } from "./session-description-handler";
 import { UA } from "./ua";
 import { NameAddrHeader } from "./name-addr-header";
-import { ReferServerContext, URI } from "."; // TODO Fix this
+import { URI } from "./uri";
 
 /**
   * The Session interface SIP.js is providing.
@@ -150,4 +150,40 @@ export namespace InviteServerContext {
   }
 }
 
+export class ReferServerContext extends EventEmitter {
 
+  /** Parsed Refer-to header. */
+  referTo?: any;
+
+  /** If followRefer is was true, the new Session post accepting REFER request. */
+  targetSession?: Session;
+
+  /** Accept the REFER request with a "202 Accepted" reply. */
+  accept(options?: ReferServerContext.AcceptOptions, modifiers?: SessionDescriptionHandlerModifiers): void;
+
+  /** Send a "100 Trying" reply. */
+  progress(): void;
+
+  /** Reject the REFER request. */
+  reject(options?: ReferServerContext.RejectOptions)
+
+  on(name: 'referRequestRejected', callback: (referServerContext: ReferServerContext) => void): void;
+  on(name: 'referRequestAccepted', callback: (referServerContext: ReferServerContext) => void): void;
+  on(name: 'referInviteSent', callback: (referServerContext: ReferServerContext) => void): void;
+  on(name: 'referProgress', callback: (referServerContext: ReferServerContext) => void): void;
+  on(name: 'referAccepted', callback: (referServerContext: ReferServerContext) => void): void;
+  on(name: 'referRejected', callback: (referServerContext: ReferServerContext) => void): void;
+}
+
+export namespace ReferServerContext {
+
+  export interface AcceptOptions {
+    /** If true, accept REFER request and automatically attempt to follow it. */
+    followRefer?: boolean;
+    /** If followRefer is true, options to following INVITE request. */
+    inviteOptions: InviteClientContext.Options;
+  }
+
+  export interface RejectOptions {
+  }
+}
