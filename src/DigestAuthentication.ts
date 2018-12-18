c1d90adbbad30a1b8806db5bd69a1f19ba7a1e8c
James Criscuolo <james@onsip.com>
2018-12-18 09:45:07 -0500

    typing update: DigestAuthentication
    - added crypto-js.d.ts to mock type and satisfy typescript

--- src/DigestAuthentication.ts
+++ src/DigestAuthentication.ts
@@ -1,171 +1,182 @@
-"use strict";
+import MD5 from "crypto-js/md5";
 
-var md5 = require('crypto-js/md5');
+import { DigestAuthentication as DigestAuthenticationDefinition } from "../types/digest-authentication";
+import { Logger } from "../types/logger-factory";
+import { OutgoingRequest } from "../types/sip-message";
+import { UA } from "../types/ua";
+import { URI } from "../types/uri";
 
-/**
- * @fileoverview SIP Digest Authentication
- */
+import { TypeStrings } from "./Enums";
+import { Utils } from "./Utils";
 
 /**
  * SIP Digest Authentication.
- * @augments SIP.
  * @function Digest Authentication
  * @param {SIP.UA} ua
  */
-module.exports = function (Utils) {
-var DigestAuthentication;
 
-DigestAuthentication = function(ua) {
-  this.logger = ua.getLogger('sipjs.digestauthentication');
-  this.username = ua.configuration.authorizationUser;
-  this.password = ua.configuration.password;
-  this.cnonce = null;
-  this.nc = 0;
-  this.ncHex = '00000000';
-  this.response = null;
-};
+export class DigestAuthentication implements DigestAuthenticationDefinition {
+  public type: TypeStrings;
+  public stale: boolean | undefined;
 
+  private logger: Logger;
+  private username: string | undefined;
+  private password: string | undefined;
+  private cnonce: string | undefined;
+  private nc: number;
+  private ncHex: string;
+  private response: any | undefined; // CryptoJS.WordArray
+  private algorithm: string | undefined;
+  private realm: string | undefined;
+  private nonce: string | undefined;
+  private opaque: string | undefined;
+  private qop: string | undefined;
+  private method: string | undefined;
+  private uri: string | URI | undefined;
 
-/**
-* Performs Digest authentication given a SIP request and the challenge
-* received in a response to that request.
-* Returns true if credentials were successfully generated, false otherwise.
-*
-* @param {SIP.OutgoingRequest} request
-* @param {Object} challenge
-*/
-DigestAuthentication.prototype.authenticate = function(request, challenge) {
-  // Inspect and validate the challenge.
-
-  this.algorithm = challenge.algorithm;
-  this.realm = challenge.realm;
-  this.nonce = challenge.nonce;
-  this.opaque = challenge.opaque;
-  this.stale = challenge.stale;
-
-  if (this.algorithm) {
-    if (this.algorithm !== 'MD5') {
-      this.logger.warn('challenge with Digest algorithm different than "MD5", authentication aborted');
-      return false;
-    }
-  } else {
-    this.algorithm = 'MD5';
+  constructor(ua: UA) {
+    this.type = TypeStrings.DigestAuthentication;
+    this.logger = ua.getLogger("sipjs.digestauthentication");
+    this.username = ua.configuration.authorizationUser;
+    this.password = ua.configuration.password;
+    this.nc = 0;
+    this.ncHex = "00000000";
   }
 
-  if (! this.realm) {
-    this.logger.warn('challenge without Digest realm, authentication aborted');
-    return false;
-  }
+  /**
+   * Performs Digest authentication given a SIP request and the challenge
+   * received in a response to that request.
+   * Returns true if credentials were successfully generated, false otherwise.
+   *
+   * @param {SIP.OutgoingRequest} request
+   * @param {Object} challenge
+   */
+  public authenticate(request: OutgoingRequest, challenge: any, body?: string): boolean {
+    // Inspect and validate the challenge.
 
-  if (! this.nonce) {
-    this.logger.warn('challenge without Digest nonce, authentication aborted');
-    return false;
-  }
+    this.algorithm = challenge.algorithm;
+    this.realm = challenge.realm;
+    this.nonce = challenge.nonce;
+    this.opaque = challenge.opaque;
+    this.stale = challenge.stale;
 
-  // 'qop' can contain a list of values (Array). Let's choose just one.
-  if (challenge.qop) {
-    if (challenge.qop.indexOf('auth') > -1) {
-      this.qop = 'auth';
-    } else if (challenge.qop.indexOf('auth-int') > -1) {
-      this.qop = 'auth-int';
+    if (this.algorithm) {
+      if (this.algorithm !== "MD5") {
+        this.logger.warn("challenge with Digest algorithm different than 'MD5', authentication aborted");
+        return false;
+      }
     } else {
-      // Otherwise 'qop' is present but does not contain 'auth' or 'auth-int', so abort here.
-      this.logger.warn('challenge without Digest qop different than "auth" or "auth-int", authentication aborted');
+      this.algorithm = "MD5";
+    }
+
+    if (!this.realm) {
+      this.logger.warn("challenge without Digest realm, authentication aborted");
       return false;
     }
-  } else {
-    this.qop = null;
+
+    if (! this.nonce) {
+      this.logger.warn("challenge without Digest nonce, authentication aborted");
+      return false;
+    }
+
+    // 'qop' can contain a list of values (Array). Let's choose just one.
+    if (challenge.qop) {
+      if (challenge.qop.indexOf("auth") > -1) {
+        this.qop = "auth";
+      } else if (challenge.qop.indexOf("auth-int") > -1) {
+        this.qop = "auth-int";
+      } else {
+        // Otherwise 'qop' is present but does not contain 'auth' or 'auth-int', so abort here.
+        this.logger.warn("challenge without Digest qop different than 'auth' or 'auth-int', authentication aborted");
+        return false;
+      }
+    } else {
+      this.qop = undefined;
+    }
+
+    // Fill other attributes.
+
+    this.method = request.method;
+    this.uri = request.ruri;
+    this.cnonce = Utils.createRandomToken(12);
+    this.nc += 1;
+    this.updateNcHex();
+
+    // nc-value = 8LHEX. Max value = 'FFFFFFFF'.
+    if (this.nc === 4294967296) {
+      this.nc = 1;
+      this.ncHex = "00000001";
+    }
+
+    // Calculate the Digest "response" value.
+    this.calculateResponse(body);
+
+    return true;
   }
 
-  // Fill other attributes.
+  /**
+   * Return the Proxy-Authorization or WWW-Authorization header value.
+   */
+  public toString(): string {
+    const authParams: Array<string> = [];
 
-  this.method = request.method;
-  this.uri = request.ruri;
-  this.cnonce = Utils.createRandomToken(12);
-  this.nc += 1;
-  this.updateNcHex();
+    if (! this.response) {
+      throw new Error("response field does not exist, cannot generate Authorization header");
+    }
 
-  // nc-value = 8LHEX. Max value = 'FFFFFFFF'.
-  if (this.nc === 4294967296) {
-    this.nc = 1;
-    this.ncHex = '00000001';
+    authParams.push("algorithm=" + this.algorithm);
+    authParams.push('username="' + this.username + '"');
+    authParams.push('realm="' + this.realm + '"');
+    authParams.push('nonce="' + this.nonce + '"');
+    authParams.push('uri="' + this.uri + '"');
+    authParams.push('response="' + this.response + '"');
+    if (this.opaque) {
+      authParams.push('opaque="' + this.opaque + '"');
+    }
+    if (this.qop) {
+      authParams.push("qop=" + this.qop);
+      authParams.push('cnonce="' + this.cnonce + '"');
+      authParams.push("nc=" + this.ncHex);
+    }
+
+    return "Digest " + authParams.join(", ");
+  }
+  /**
+   * Generate the 'nc' value as required by Digest in this.ncHex by reading this.nc.
+   * @private
+   */
+  private updateNcHex(): void {
+    const hex = Number(this.nc).toString(16);
+    this.ncHex = "00000000".substr(0, 8 - hex.length) + hex;
   }
 
-  // Calculate the Digest "response" value.
-  this.calculateResponse();
+  /**
+   * Generate Digest 'response' value.
+   * @private
+   */
+  private calculateResponse(body?: string): void {
+    let ha2;
 
-  return true;
-};
+    // HA1 = MD5(A1) = MD5(username:realm:password)
+    const ha1 = MD5(this.username + ":" + this.realm + ":" + this.password);
 
+    if (this.qop === "auth") {
+      // HA2 = MD5(A2) = MD5(method:digestURI)
+      ha2 = MD5(this.method + ":" + this.uri);
+      // response = MD5(HA1:nonce:nonceCount:credentialsNonce:qop:HA2)
+      this.response = MD5(ha1 + ":" + this.nonce + ":" + this.ncHex + ":" + this.cnonce + ":auth:" + ha2);
 
-/**
-* Generate Digest 'response' value.
-* @private
-*/
-DigestAuthentication.prototype.calculateResponse = function() {
-  var ha1, ha2;
+    } else if (this.qop === "auth-int") {
+      // HA2 = MD5(A2) = MD5(method:digestURI:MD5(entityBody))
+      ha2 = MD5(this.method + ":" + this.uri + ":" + MD5(body ? body : ""));
+      // response = MD5(HA1:nonce:nonceCount:credentialsNonce:qop:HA2)
+      this.response = MD5(ha1 + ":" + this.nonce + ":" + this.ncHex + ":" + this.cnonce + ":auth-int:" + ha2);
 
-  // HA1 = MD5(A1) = MD5(username:realm:password)
-  ha1 = md5(this.username + ":" + this.realm + ":" + this.password);
-
-  if (this.qop === 'auth') {
-    // HA2 = MD5(A2) = MD5(method:digestURI)
-    ha2 = md5(this.method + ":" + this.uri);
-    // response = MD5(HA1:nonce:nonceCount:credentialsNonce:qop:HA2)
-    this.response = md5(ha1 + ":" + this.nonce + ":" + this.ncHex + ":" + this.cnonce + ":auth:" + ha2);
-
-  } else if (this.qop === 'auth-int') {
-    // HA2 = MD5(A2) = MD5(method:digestURI:MD5(entityBody))
-    ha2 = md5(this.method + ":" + this.uri + ":" + md5(this.body ? this.body : ""));
-    // response = MD5(HA1:nonce:nonceCount:credentialsNonce:qop:HA2)
-    this.response = md5(ha1 + ":" + this.nonce + ":" + this.ncHex + ":" + this.cnonce + ":auth-int:" + ha2);
-
-  } else if (this.qop === null) {
-    // HA2 = MD5(A2) = MD5(method:digestURI)
-    ha2 = md5(this.method + ":" + this.uri);
-    // response = MD5(HA1:nonce:HA2)
-    this.response = md5(ha1 + ":" + this.nonce + ":" + ha2);
+    } else if (this.qop === undefined) {
+      // HA2 = MD5(A2) = MD5(method:digestURI)
+      ha2 = MD5(this.method + ":" + this.uri);
+      // response = MD5(HA1:nonce:HA2)
+      this.response = MD5(ha1 + ":" + this.nonce + ":" + ha2);
+    }
   }
-};
-
-
-/**
-* Return the Proxy-Authorization or WWW-Authorization header value.
-*/
-DigestAuthentication.prototype.toString = function() {
-  var auth_params = [];
-
-  if (! this.response) {
-    throw new Error('response field does not exist, cannot generate Authorization header');
-  }
-
-  auth_params.push('algorithm=' + this.algorithm);
-  auth_params.push('username="' + this.username + '"');
-  auth_params.push('realm="' + this.realm + '"');
-  auth_params.push('nonce="' + this.nonce + '"');
-  auth_params.push('uri="' + this.uri + '"');
-  auth_params.push('response="' + this.response + '"');
-  if (this.opaque) {
-    auth_params.push('opaque="' + this.opaque + '"');
-  }
-  if (this.qop) {
-    auth_params.push('qop=' + this.qop);
-    auth_params.push('cnonce="' + this.cnonce + '"');
-    auth_params.push('nc=' + this.ncHex);
-  }
-
-  return 'Digest ' + auth_params.join(', ');
-};
-
-
-/**
-* Generate the 'nc' value as required by Digest in this.ncHex by reading this.nc.
-* @private
-*/
-DigestAuthentication.prototype.updateNcHex = function() {
-  var hex = Number(this.nc).toString(16);
-  this.ncHex = '00000000'.substr(0, 8-hex.length) + hex;
-};
-
-return DigestAuthentication;
-};
+}
