c1a7b35d72736c90b03879cfca402d440ba32716
James Criscuolo <james@onsip.com>
2018-12-18 09:45:38 -0500

    typing update: ServerContext

--- src/ServerContext.ts
+++ src/ServerContext.ts
@@ -1,93 +1,127 @@
-"use strict";
-module.exports = function (SIP) {
-var ServerContext;
+import { EventEmitter } from "events";
 
-ServerContext = function (ua, request) {
-  this.ua = ua;
-  this.logger = ua.getLogger('sip.servercontext');
-  this.request = request;
-  if (request.method === SIP.C.INVITE) {
-    this.transaction = new SIP.Transactions.InviteServerTransaction(request, ua);
-  } else {
-    this.transaction = new SIP.Transactions.NonInviteServerTransaction(request, ua);
+import { Logger } from "../types/logger-factory";
+import { NameAddrHeader } from "../types/name-addr-header";
+import { ServerContext as ServerContextDefinition } from "../types/server-context";
+import { IncomingRequest } from "../types/sip-message";
+import {
+  InviteServerTransaction as InviteServerTransactionType,
+  NonInviteServerTransaction as NonInviteServerTransactionType
+} from "../types/transactions";
+import { UA } from "../types/ua";
+
+import { C } from "./Constants";
+import { TypeStrings } from "./Enums";
+import { Grammar } from "./Grammar";
+import { InviteServerTransaction, NonInviteServerTransaction } from "./Transactions";
+import { Utils } from "./Utils";
+
+export class ServerContext extends EventEmitter implements ServerContextDefinition {
+  // hack to get around our multiple inheritance issues
+  public static initializer(objectToConstruct: ServerContext, ua: UA, request: IncomingRequest): void {
+    objectToConstruct.type = TypeStrings.ServerContext;
+    objectToConstruct.ua = ua;
+    objectToConstruct.logger = ua.getLogger("sip.servercontext");
+    objectToConstruct.request = request;
+    if (request.method === C.INVITE) {
+      objectToConstruct.transaction = new InviteServerTransaction(request, ua);
+    } else {
+      objectToConstruct.transaction = new NonInviteServerTransaction(request, ua);
+    }
+
+    if (request.body) {
+      objectToConstruct.body = request.body;
+    }
+    if (request.hasHeader("Content-Type")) {
+      objectToConstruct.contentType = request.getHeader("Content-Type");
+    }
+    objectToConstruct.method = request.method;
+
+    objectToConstruct.localIdentity = request.to;
+    objectToConstruct.remoteIdentity = request.from;
+    const hasAssertedIdentity = request.hasHeader("P-Asserted-Identity");
+    if (hasAssertedIdentity) {
+      const assertedIdentity: string | undefined = request.getHeader("P-Asserted-Identity");
+      if (assertedIdentity) {
+        objectToConstruct.assertedIdentity = Grammar.nameAddrHeaderParse(assertedIdentity);
+      }
+    }
   }
 
-  if (request.body) {
-    this.body = request.body;
+  public type!: TypeStrings;
+  public ua!: UA;
+  public logger!: Logger;
+  public localIdentity!: NameAddrHeader;
+  public remoteIdentity!: NameAddrHeader;
+  public method!: string;
+  public request!: IncomingRequest;
+  public data: any = {};
+
+  // Typing note: these were all private, needed to switch to get around
+  // inheritance issue with InviteServerContext
+  public transaction!: InviteServerTransactionType | NonInviteServerTransactionType;
+  public body: any;
+  public contentType: string | undefined;
+  public assertedIdentity: NameAddrHeader | undefined;
+
+  constructor(ua: UA, request: IncomingRequest) {
+    super();
+
+    ServerContext.initializer(this, ua, request);
   }
-  if (request.hasHeader('Content-Type')) {
-    this.contentType = request.getHeader('Content-Type');
+
+  public progress(options: any = {}): any {
+    options.statusCode = options.statusCode || 180;
+    options.minCode = 100;
+    options.maxCode = 199;
+    options.events = ["progress"];
+
+    return this.reply(options);
   }
-  this.method = request.method;
 
-  this.data = {};
+  public accept(options: any = {}): any {
+    options.statusCode = options.statusCode || 200;
+    options.minCode = 200;
+    options.maxCode = 299;
+    options.events = ["accepted"];
 
-  this.localIdentity = request.to;
-  this.remoteIdentity = request.from;
-  if (request.hasHeader('P-Asserted-Identity')) {
-    this.assertedIdentity = new SIP.NameAddrHeader.parse(request.getHeader('P-Asserted-Identity'));
+    return this.reply(options);
   }
-};
 
-ServerContext.prototype = Object.create(SIP.EventEmitter.prototype);
+  public reject(options: any = {}): any {
+    options.statusCode = options.statusCode || 480;
+    options.minCode = 300;
+    options.maxCode = 699;
+    options.events = ["rejected", "failed"];
 
-ServerContext.prototype.progress = function (options) {
-  options = Object.create(options || Object.prototype);
-  options.statusCode || (options.statusCode = 180);
-  options.minCode = 100;
-  options.maxCode = 199;
-  options.events = ['progress'];
-  return this.reply(options);
-};
-
-ServerContext.prototype.accept = function (options) {
-  options = Object.create(options || Object.prototype);
-  options.statusCode || (options.statusCode = 200);
-  options.minCode = 200;
-  options.maxCode = 299;
-  options.events = ['accepted'];
-  return this.reply(options);
-};
-
-ServerContext.prototype.reject = function (options) {
-  options = Object.create(options || Object.prototype);
-  options.statusCode || (options.statusCode = 480);
-  options.minCode = 300;
-  options.maxCode = 699;
-  options.events = ['rejected', 'failed'];
-  return this.reply(options);
-};
-
-ServerContext.prototype.reply = function (options) {
-  options = options || {}; // This is okay, so long as we treat options as read-only in this method
-  var
-    statusCode = options.statusCode || 100,
-    minCode = options.minCode || 100,
-    maxCode = options.maxCode || 699,
-    reasonPhrase = SIP.Utils.getReasonPhrase(statusCode, options.reasonPhrase),
-    extraHeaders = options.extraHeaders || [],
-    body = options.body,
-    events = options.events || [],
-    response;
-
-  if (statusCode < minCode || statusCode > maxCode) {
-    throw new TypeError('Invalid statusCode: ' + statusCode);
+    return this.reply(options);
   }
-  response = this.request.reply(statusCode, reasonPhrase, extraHeaders, body);
-  events.forEach(function (event) {
-    this.emit(event, response, reasonPhrase);
-  }, this);
 
-  return this;
-};
+  public reply(options: any = {}): any {
+    const statusCode = options.statusCode || 100;
+    const minCode = options.minCode || 100;
+    const maxCode = options.maxCode || 699;
+    const reasonPhrase = Utils.getReasonPhrase(statusCode, options.reasonPhrase);
+    const extraHeaders = options.extraHeaders || [];
+    const body = options.body;
+    const events: Array<string> = options.events || [];
 
-ServerContext.prototype.onRequestTimeout = function () {
-  this.emit('failed', null, SIP.C.causes.REQUEST_TIMEOUT);
-};
+    if (statusCode < minCode || statusCode > maxCode) {
+      throw new TypeError("Invalid statusCode: " + statusCode);
+    }
+    const response = this.request.reply(statusCode, reasonPhrase, extraHeaders, body);
+    events.forEach((event) => {
+      this.emit(event, response, reasonPhrase);
+    });
 
-ServerContext.prototype.onTransportError = function () {
-  this.emit('failed', null, SIP.C.causes.CONNECTION_ERROR);
-};
+    return this;
+  }
 
-SIP.ServerContext = ServerContext;
-};
+  public onRequestTimeout(): void {
+    this.emit("failed", undefined, C.causes.REQUEST_TIMEOUT);
+  }
+
+  public onTransportError(): void {
+    this.emit("failed", undefined, C.causes.CONNECTION_ERROR);
+  }
+}
