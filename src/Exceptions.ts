5fabbd4bfa50f3bf45c06ced6c60ac7b95ecfc6e
James Criscuolo <james@onsip.com>
2018-12-18 12:12:42 -0500

    typing update: Exceptions

--- src/Exceptions.ts
+++ src/Exceptions.ts
@@ -1,99 +1,98 @@
-"use strict";
-/**
- * @fileoverview Exceptions
- */
+import {
+  Exception as ExceptionDefinition,
+  Exceptions as ExceptionsDefinition
+} from "../types/exceptions";
 
-/**
- * SIP Exceptions.
- * @augments SIP
- */
-module.exports = {
-  ConfigurationError: (function(){
-    var exception = function(parameter, value) {
-      this.code = 1;
-      this.name = 'CONFIGURATION_ERROR';
+import { SessionStatus, TypeStrings } from "./Enums";
+
+// tslint:disable:max-classes-per-file
+abstract class Exception extends Error implements ExceptionDefinition {
+  public type!: TypeStrings;
+  public name: string;
+  public message: string;
+  public code: number;
+
+  constructor(code: number, name: string, message: string) {
+    super(message);
+    this.code = code;
+    this.name = name;
+    this.message = message;
+  }
+}
+
+export namespace Exceptions {
+  export class ConfigurationError extends Exception implements ExceptionsDefinition.ConfigurationError {
+    public parameter: string;
+    public value: any;
+
+    constructor(parameter: string, value?: any) {
+      super(1, "CONFIGURATION_ERROR", (!value) ? "Missing parameter: " + parameter :
+      "Invalid value " + JSON.stringify(value) + " for parameter '" + parameter + "'");
+      this.type = TypeStrings.ConfigurationError;
       this.parameter = parameter;
       this.value = value;
-      this.message = (!this.value)? 'Missing parameter: '+ this.parameter : 'Invalid value '+ JSON.stringify(this.value) +' for parameter "'+ this.parameter +'"';
-    };
-    exception.prototype = new Error();
-    return exception;
-  }()),
+    }
+  }
 
-  InvalidStateError: (function(){
-    var exception = function(status) {
-      this.code = 2;
-      this.name = 'INVALID_STATE_ERROR';
+  export class InvalidStateError extends Exception implements ExceptionsDefinition.InvalidStateError {
+    public status: SessionStatus;
+
+    constructor(status: SessionStatus) {
+      super(2, "INVALID_STATE_ERROR", "Invalid status: " + status);
+      this.type = TypeStrings.InvalidStateError;
       this.status = status;
-      this.message = 'Invalid status: ' + status;
-    };
-    exception.prototype = new Error();
-    return exception;
-  }()),
+    }
+  }
 
-  NotSupportedError: (function(){
-    var exception = function(message) {
-      this.code = 3;
-      this.name = 'NOT_SUPPORTED_ERROR';
-      this.message = message;
-    };
-    exception.prototype = new Error();
-    return exception;
-  }()),
+  export class NotSupportedError extends Exception implements ExceptionsDefinition. NotSupportedError {
+    constructor(message: string) {
+      super(3, "NOT_SUPPORTED_ERROR", message);
+      this.type = TypeStrings.NotSupportedError;
+    }
+  }
 
-  // Deprecated
-  GetDescriptionError: (function(){
-    var exception = function(message) {
-      this.code = 4;
-      this.name = 'GET_DESCRIPTION_ERROR';
-      this.message = message;
-    };
-    exception.prototype = new Error();
-    return exception;
-  }()),
+  // 4 was GetDescriptionError, which was deprecated and now removed
 
-  RenegotiationError: (function(){
-    var exception = function(message) {
-      this.code = 5;
-      this.name = 'RENEGOTIATION_ERROR';
-      this.message = message;
-    };
-    exception.prototype = new Error();
-    return exception;
-  }()),
+  export class RenegotiationError extends Exception implements ExceptionsDefinition.RenegotiationError {
+    constructor(message: string) {
+      super(5, "RENEGOTIATION_ERROR", message);
+      this.type = TypeStrings.RenegotiationError;
+    }
+  }
 
-  MethodParameterError: (function(){
-    var exception = function(method, parameter, value) {
-      this.code = 6;
-      this.name = 'METHOD_PARAMETER_ERROR';
+  export class MethodParameterError extends Exception implements ExceptionsDefinition.MethodParameterError {
+    public method: string;
+    public parameter: string;
+    public value: any;
+
+    constructor(method: string, parameter: string, value: any) {
+      super(6, "METHOD_PARAMETER_ERROR", (!value) ?
+        "Missing parameter: " + parameter :
+        "Invalid value " + JSON.stringify(value) + " for parameter '" + parameter + "'");
+      this.type = TypeStrings.MethodParameterError;
       this.method = method;
       this.parameter = parameter;
       this.value = value;
-      this.message = (!this.value)? 'Missing parameter: '+ this.parameter : 'Invalid value '+ JSON.stringify(this.value) +' for parameter "'+ this.parameter +'"';
-    };
-    exception.prototype = new Error();
-    return exception;
-  }()),
+    }
+  }
 
-  TransportError: (function(){
-    var exception = function(message) {
-      this.code = 7;
-      this.name = 'TRANSPORT_ERROR';
-      this.message = message;
-    };
-    exception.prototype = new Error();
-    return exception;
-  }()),
+  export class TransportError extends Exception implements ExceptionsDefinition.TransportError {
+    constructor(message: string) {
+      super(7, "TRANSPORT_ERROR", message);
+      this.type = TypeStrings.TransportError;
+    }
+  }
 
-  SessionDescriptionHandlerError: (function(){
-    var exception = function(method, error, message) {
-      this.code = 8;
-      this.name = 'SESSION_DESCRIPTION_HANDLER_ERROR';
+  export class SessionDescriptionHandlerError extends Exception
+    implements ExceptionsDefinition.SessionDescriptionHandlerError {
+    public error: string | undefined;
+    public method: string;
+
+    constructor(method: string, error?: string, message?: string) {
+      super(8, "SESSION_DESCRIPTION_HANDLER_ERROR", message || "Error with Session Description Handler");
+      this.type = TypeStrings.SessionDescriptionHandlerError;
       this.method = method;
       this.error = error;
-      this.message = message || 'Error with Session Description Handler';
-    };
-    exception.prototype = new Error();
-    return exception;
-  }()),
-};
+    }
+  }
+}
