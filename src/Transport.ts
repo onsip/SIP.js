f1112a003f79af27f2ba8bc8c067e0d44e4fd6cf
James Criscuolo <james@onsip.com>
2018-12-26 11:37:25 -0500

    typing update: Transport

--- src/Transport.ts
+++ src/Transport.ts
@@ -1,114 +1,135 @@
-"use strict";
-/* eslint-disable */
-/**
- * @fileoverview Transport
- */
+import { EventEmitter } from "events";
+
+import { Logger } from "../types/logger-factory";
+import { OutgoingRequest } from "../types/sip-message";
+import { Transport as TransportDefinition } from "../types/transport";
+
+import { TypeStrings } from "./Enums";
 
 /* Transport
  * @class Abstract transport layer parent class
  * @param {Logger} logger
  * @param {Object} [options]
  */
-module.exports = function (SIP) {
-var Transport = function(logger, options) {};
+export abstract class Transport extends EventEmitter implements TransportDefinition {
+  public type: TypeStrings;
+  public server: any;
+  protected logger: Logger;
 
-Transport.prototype = Object.create(SIP.EventEmitter.prototype, {
+  constructor(logger: Logger, options: any) {
+    super();
+    this.type = TypeStrings.Transport;
 
-  /**
-  * Returns the promise designated by the child layer then emits a connected event. Automatically emits an event upon resolution, unless overrideEvent is set. If you override the event in this fashion, you should emit it in your implementation of connectPromise
-  * @param {Object} [options]
-  * @returns {Promise}
-  */
-  connect: {writable: true, value: function connect (options) {
-    options = options || {};
-    return this.connectPromise(options).then(function (data) {!data.overrideEvent && this.emit('connected');}.bind(this));
-  }},
+    this.logger = logger;
+  }
 
   /**
-  * Called by connect, must return a promise
-  * promise must resolve to an object. object supports 1 parameter: overrideEvent - Boolean
-  * @abstract
-  * @private
-  * @param {Object} [options]
-  * @returns {Promise}
-  */
-  connectPromise: {writable: true, value: function connectPromise (options) {}},
+   * Returns the promise designated by the child layer then emits a connected event.
+   * Automatically emits an event upon resolution, unless overrideEvent is set. If you
+   * override the event in this fashion, you should emit it in your implementation of connectPromise
+   * @param {Object} [options]
+   * @returns {Promise}
+   */
+  public connect(options: any = {}): Promise<any> {
+    return this.connectPromise(options).then((data: any) => {
+      if (!data.overrideEvent) {
+        this.emit("connected");
+      }
+    });
+  }
 
   /**
-  * Returns true if the transport is connected
-  * @abstract
-  * @returns {Boolean}
-  */
-  isConnected: {writable: true, value: function isConnected () {}},
+   * Returns true if the transport is connected
+   * @returns {Boolean}
+   */
+  public abstract isConnected(): boolean;
 
   /**
-  * Sends a message then emits a 'messageSent' event. Automatically emits an event upon resolution, unless data.overrideEvent is set. If you override the event in this fashion, you should emit it in your implementation of sendPromise
-  * @param {SIP.OutgoingRequest|String} msg
-  * @param {Object} options
-  * @returns {Promise}
-  */
-  send: {writable: true, value: function send (msg, options) {
-    options = options || {};
-    return this.sendPromise(msg).then(function (data) {!data.overrideEvent && this.emit('messageSent', data.msg);}.bind(this));
-  }},
+   * Sends a message then emits a 'messageSent' event. Automatically emits an
+   * event upon resolution, unless data.overrideEvent is set. If you override
+   * the event in this fashion, you should emit it in your implementation of sendPromise
+   * @param {SIP.OutgoingRequest|String} msg
+   * @param {Object} options
+   * @returns {Promise}
+   */
+  public send(msg: OutgoingRequest | string, options: any = {}): Promise<any> {
+    return this.sendPromise(msg).then((data: any) => {
+      if (!data.overrideEvent) {
+        this.emit("messageSent", data.msg);
+      }
+    });
+  }
 
   /**
-  * Called by send, must return a promise
-  * promise must resolve to an object. object supports 2 parameters: msg - string (mandatory) and overrideEvent - Boolean (optional)
-  * @abstract
-  * @private
-  * @param {SIP.OutgoingRequest|String} msg
-  * @param {Object} [options]
-  * @returns {Promise}
-  */
-  sendPromise: {writable: true, value: function sendPromise (msg, options) {}},
+   * Returns the promise designated by the child layer then emits a
+   * disconnected event. Automatically emits an event upon resolution,
+   * unless overrideEvent is set. If you override the event in this fashion,
+   * you should emit it in your implementation of disconnectPromise
+   * @param {Object} [options]
+   * @returns {Promise}
+   */
+  public disconnect(options: any = {}): Promise<any> {
+    return this.disconnectPromise(options).then((data: any) => {
+      if (!data.overrideEvent) {
+        this.emit("disconnected");
+      }
+    });
+  }
 
-  /**
-  * To be called when a message is received
-  * @abstract
-  * @param {Event} e
-  */
-  onMessage: {writable: true, value: function onMessage (e) {}},
-
-  /**
-  * Returns the promise designated by the child layer then emits a disconnected event. Automatically emits an event upon resolution, unless overrideEvent is set. If you override the event in this fashion, you should emit it in your implementation of disconnectPromise
-  * @param {Object} [options]
-  * @returns {Promise}
-  */
-  disconnect: {writable: true, value: function disconnect (options) {
-    options = options || {};
-    return this.disconnectPromise(options).then(function (data) {!data.overrideEvent && this.emit('disconnected');}.bind(this));
-  }},
-
-  /**
-  * Called by disconnect, must return a promise
-  * promise must resolve to an object. object supports 1 parameter: overrideEvent - Boolean
-  * @abstract
-  * @private
-  * @param {Object} [options]
-  * @returns {Promise}
-  */
-  disconnectPromise: {writable: true, value: function disconnectPromise (options) {}},
-
-  afterConnected: {writable: true, value: function afterConnected (callback) {
+  public afterConnected(callback: () => void): void {
     if (this.isConnected()) {
       callback();
     } else {
-      this.once('connected', callback);
+      this.once("connected", callback);
     }
-  }},
+  }
 
   /**
    * Returns a promise which resolves once the UA is connected. DEPRECATION WARNING: just use afterConnected()
    * @returns {Promise}
    */
-  waitForConnected: {writable: true, value: function waitForConnected () {
+  public waitForConnected(): Promise<any> {
+    // tslint:disable-next-line:no-console
     console.warn("DEPRECATION WARNING Transport.waitForConnected(): use afterConnected() instead");
-    return new Promise(function(resolve) {
+    return new Promise((resolve) => {
       this.afterConnected(resolve);
-    }.bind(this));
-  }},
-});
+    });
+  }
+  /**
+   * Called by connect, must return a promise
+   * promise must resolve to an object. object supports 1 parameter: overrideEvent - Boolean
+   * @abstract
+   * @private
+   * @param {Object} [options]
+   * @returns {Promise}
+   */
+  protected abstract connectPromise(options: any): Promise<any>;
 
-return Transport;
-};
+  /**
+   * Called by send, must return a promise
+   * promise must resolve to an object. object supports 2 parameters: msg - string (mandatory)
+   * and overrideEvent - Boolean (optional)
+   * @abstract
+   * @private
+   * @param {SIP.OutgoingRequest|String} msg
+   * @param {Object} [options]
+   * @returns {Promise}
+   */
+  protected abstract sendPromise(msg: OutgoingRequest | string, options?: any): Promise<any>;
+
+  /**
+   * Called by disconnect, must return a promise
+   * promise must resolve to an object. object supports 1 parameter: overrideEvent - Boolean
+   * @abstract
+   * @private
+   * @param {Object} [options]
+   * @returns {Promise}
+   */
+  protected abstract disconnectPromise(options: any): Promise<any>;
+
+  /**
+   * To be called when a message is received
+   * @param {Event} e
+   */
+  protected abstract onMessage(e: any): void;
+}
