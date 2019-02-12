148f1cb13e023428915e735a545ac52cceaf00ac
James Criscuolo <james@onsip.com>
2019-02-12 14:30:56 -0500

    DTMF: fix sending dtmf over INFO during a call in which you are the UAS (the less common case)

--- types/Session/dtmf.d.ts
+++ types/Session/dtmf.d.ts
@@ -1,7 +1,7 @@
 import { EventEmitter } from "events";
 
 import { Session } from "../session";
-import { IncomingRequest } from "../sip-message";
+import { IncomingRequest, IncomingResponse } from "../sip-message";
 
 export declare class DTMF extends EventEmitter {
   tone: string;
@@ -11,5 +11,9 @@ export declare class DTMF extends EventEmitter {
   constructor(session: Session, tone?: string | number, options?: any);
 
   send(options?: any): void;
-  init_incoming(request: IncomingRequest): void
+  init_incoming(request: IncomingRequest): void;
+  receiveResponse(response: IncomingResponse): void;
+  onRequestTimeout(): void;
+  onTransportError(): void;
+  onDialogError(response: IncomingResponse): void;
 }
