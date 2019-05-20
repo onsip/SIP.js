c03bd3254cd645723fdf6488d9fef59b09495416
James Criscuolo <james@onsip.com>
2019-05-20 11:22:57 -0400

    Revert "dist/ lib/ files for 0.13.8"
    This reverts commit 2a672b5974c6c39c2213938038539afc04d46299.

--- lib/session-description-handler-observer.d.ts
+++
@@ -1,8 +0,0 @@
-import { TypeStrings } from "./Enums";
-import { InviteClientContext, InviteServerContext } from "./Session";
-export declare class SessionDescriptionHandlerObserver {
-    type: TypeStrings;
-    constructor(session: InviteClientContext | InviteServerContext, options: any);
-    trackAdded(): void;
-    directionChanged(): void;
-}
