bf51bcbdb29ba94f02284a15eb559f0c1aee9064
James Criscuolo <james@onsip.com>
2019-06-18 12:13:18 -0400

    Revert "dist/ lib/ files for 0.14.5"
    This reverts commit 29f4e1223c40dd39e53c0fb36ec238dccad9b8c7.

--- lib/core/user-agents/info-user-agent-client.d.ts
+++
@@ -1,6 +0,0 @@
-import { SessionDialog } from "../dialogs";
-import { OutgoingInfoRequest, OutgoingRequestDelegate, RequestOptions } from "../messages";
-import { UserAgentClient } from "./user-agent-client";
-export declare class InfoUserAgentClient extends UserAgentClient implements OutgoingInfoRequest {
-    constructor(dialog: SessionDialog, delegate?: OutgoingRequestDelegate, options?: RequestOptions);
-}
