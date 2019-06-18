c3f995d607d7309163d0abf773f9edc905667d1b
James Criscuolo <james@onsip.com>
2019-06-18 09:17:45 -0400

    Revert "dist/ lib/ files for 0.14.4"
    This reverts commit be5d66afc8da611cfa67b10ec1b5d87c4e0b4d5d.

--- lib/core/user-agents/re-subscribe-user-agent-server.d.ts
+++
@@ -1,6 +0,0 @@
-import { Dialog } from "../dialogs";
-import { IncomingRequestDelegate, IncomingRequestMessage, IncomingSubscribeRequest } from "../messages";
-import { UserAgentServer } from "./user-agent-server";
-export declare class ReSubscribeUserAgentServer extends UserAgentServer implements IncomingSubscribeRequest {
-    constructor(dialog: Dialog, message: IncomingRequestMessage, delegate?: IncomingRequestDelegate);
-}
