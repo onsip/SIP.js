78d508ff6a1de348bbf7f62eaff49e2ac5885c14
Eric Green <eric.green@onsip.com>
2019-07-25 17:28:40 -0400

    Revert "dist/ lib/ files for 0.15.1"
    This reverts commit 7273b5289b313af7acbc980359bce8e560d3e56d.

--- lib/api/referrer-options.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Options for {@link Referrer} constructor.
- * @public
- */
-export interface ReferrerOptions {
-    extraHeaders?: Array<string>;
-}
-//# sourceMappingURL=referrer-options.d.ts.map
--- lib/api/registerer-unregister-options.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Options for {@link Registerer.unregister}.
- * @public
- */
-export interface RegistererUnregisterOptions {
-    all?: boolean;
-    extraHeaders?: Array<string>;
-}
-//# sourceMappingURL=registerer-unregister-options.d.ts.map
