6bd76ecc6b9c4ef13bb549862eb7b62d4d087ce4
Eric Green <eric.green@onsip.com>
2019-08-06 13:37:37 -0400

    Revert "dist/ lib/ files for 0.15.3"
    This reverts commit 79c1e203672a53d95e754a2bd4aa12a35adb742d.

--- lib/api/registerer-register-options.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Options for {@link Registerer.register}.
- * @public
- */
-export interface RegistererRegisterOptions {
-    closeWithHeaders?: Array<string>;
-    extraHeaders?: Array<string>;
-}
-//# sourceMappingURL=registerer-register-options.d.ts.map
