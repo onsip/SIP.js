f6b494e568b4d15bf86ac943ccb8dceee3230cc3
Cesar Armando Perez Fernandez <E1007139@ercbpo.com>
2019-11-11 15:46:38 -0400

    Moving exceptions in DTMFValidator to method.

--- src/Session/DTMFValidator.ts
+++ src/Session/DTMFValidator.ts
@@ -6,13 +6,17 @@ export class DTMFValidator {
     } else if (typeof tone === "number") {
       tone = tone.toString();
     } else {
-      throw new TypeError("Invalid tone(s): " + tone);
+      DTMFValidator.generateInvalidToneError(tone);
     }
     const regex = moreThanOneTone ? /^[0-9A-D#*,]+$/i : /^[0-9A-D#*]$/i;
     // Check tone value
     if (!tone.match(regex)) {
-      throw new TypeError("Invalid tone(s): " + tone);
+      DTMFValidator.generateInvalidToneError(tone);
     }
     return tone;
+  }
+
+  private static generateInvalidToneError(tone: string): void {
+    throw new TypeError("Invalid tone(s): " + tone.toLowerCase());
   }
 }
