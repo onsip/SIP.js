c26ae8c12b857a4be822cadbf1b4069699597fbd
John Riordan <john@onsip.com>
2019-11-14 13:17:52 -0500

    New API - add index file

---
+++ src/platform/web/index.ts
@@ -0,0 +1,8 @@
+import * as Modifiers from "./modifiers";
+export { Modifiers };
+export { SessionDescriptionHandler } from "./session-description-handler";
+export { SimpleUser } from "./simple-user";
+export { SimpleUserDelegate } from "./simple-user-delegate";
+export { SimpleUserOptions } from "./simple-user-options";
+export { Transport } from "./transport";
+export { TransportOptions } from "./transport-options";
