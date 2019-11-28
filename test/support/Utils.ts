f68bc5ff5007d546511735f0bd77c152dacfb0d9
John Riordan <john@onsip.com>
2019-11-28 11:22:18 -0500

    Version 0.16 - delete old api files

--- test/support/Utils.ts
+++
@@ -1,7 +0,0 @@
-
-export async function soon(timeout = 1): Promise<void> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(), timeout);
-    jasmine.clock().tick(timeout);
-  });
-}
