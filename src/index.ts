2b125a88f4b7b6b1197dd44f9f469a4ae4b0b0a4
John Riordan <john@onsip.com>
2019-11-28 12:00:32 -0500

    Version 0.16 - update index

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,16 @@ const version = LIBRARY_VERSION;
 const name = "sip.js";
 export { name, version };
 
-// Export entire api at root level
+// Export api
 export * from "./api";
 
-// Export selected core items at root level
-export {
-  Grammar,
-  URI
-} from "./core";
+// Export grammar
+export * from "./grammar";
 
-// Export entire core namespaced
+// Export namespaced core
 import * as Core from "./core";
 export { Core };
 
-// Export entire web namespaced
+// Export namespaced web
 import * as Web from "./platform/web";
 export { Web };
