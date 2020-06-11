f0ddb87dba78c1d6f6ce1075c51bc50f54c62e17
John Riordan <john@onsip.com>
2020-06-10 22:01:29 -0400

    Remvoe old SDH source

--- src/platform/web/session-description-handler-old/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A SessionDescriptionHandler for web browsers.
- * @packageDocumentation
- */
-export * from "./peer-connection-options";
-export * from "./session-description-handler";
-export * from "./session-description-handler-options";
