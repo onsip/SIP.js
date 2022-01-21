3e7178daa36f09839a857eeac79501bbb6d44dc6
Thadeu Esteves Jr <tadeuu@gmail.com>
2022-01-21 13:45:45 -0300

    removing lib folder

--- lib/core/messages/incoming-request-message.d.ts
+++
@@ -1,9 +0,0 @@
-import { URI } from "../messages";
-import { IncomingMessage } from "./incoming-message";
-/**
- * Incoming SIP request message.
- */
-export declare class IncomingRequestMessage extends IncomingMessage {
-    ruri: URI | undefined;
-    constructor();
-}
--- lib/core/messages/methods/register.d.ts
+++
@@ -1,9 +0,0 @@
-import { IncomingRequest } from "../incoming-request";
-import { IncomingResponse } from "../incoming-response";
-import { OutgoingRequest } from "../outgoing-request";
-export interface IncomingRegisterRequest extends IncomingRequest {
-}
-export interface IncomingRegisterResponse extends IncomingResponse {
-}
-export interface OutgoingRegisterRequest extends OutgoingRequest {
-}
