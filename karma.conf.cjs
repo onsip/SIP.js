d58ecf666a267b1d2adbae30312eac6f88b34c78
John Riordan <john-e-riordan@users.noreply.github.com>
2022-09-28 11:04:46 -0400

    Replace Travis CI with GitHub CI
    - create node.js.yml for GitHub CI
    - increase timeoutInterval in karma.conf
    - update Build/CI badge on README
    - remove travis.yml

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -72,6 +72,7 @@ module.exports = function(config) {
       clearContext: false,
       captureConsole: false,
       jasmine: {
+        timeoutInterval: 10000,
         // only necessary due to potential bug in SpecSanityCheck 8.2.2.2, running
         // those out of order causes them to fail
         random: false
